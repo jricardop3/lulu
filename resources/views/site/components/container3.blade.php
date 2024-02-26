7405d830a598cebb4e4bfd4b428d1bf894a68e77
Gabriel Martinelli <gabrielmartinellix@outlook.com>
2024-02-26 20:25:58 -0300

    Att 0.53

---
+++ resources/views/site/components/container3.blade.php
@@ -0,0 +1,12 @@
+<div style=" background: rgba(255, 255, 255, 0.47); border-radius: 16px; box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(9.4px);-webkit-backdrop-filter: blur(9.4px);
+border: 1px solid rgba(255, 255, 255, 0.38); "
+    class="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
+    <div class="container-fluid row">
+
+        <div class="row animate_animated animate_rotateOut">
+            @component('site.components.card7')
+            @endcomponent
+        </div>
+
+    </div>
+</div>

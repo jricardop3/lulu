155adf132d393a80b8b8a620ff33ae5cc170ec67
Manatocode <gabrielmartinellix@outlook.com>
2024-02-26 09:29:26 -0300

    Att 0.2

--- resources/views/site/components/container.blade.php
+++ resources/views/site/components/container.blade.php
@@ -1,12 +1,12 @@
 <div style=" background: rgba(255, 255, 255, 0.47); border-radius: 16px; box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1); backdrop-filter: blur(9.4px);-webkit-backdrop-filter: blur(9.4px);
 border: 1px solid rgba(255, 255, 255, 0.38); " class="h-100 w-100 d-flex flex-column justify-content-center align-items-center">
-  <div class="row text-center">
+  <div class="container-fluid row">
 
     <div class="col-md-6 col-sm-12 ">
       @component('site.components.imagem')
       @endcomponent
     </div>
-    <div class="container-fluid col-md-6 col-12">
+    <div class="col-md-6 col-12 ">
       @component('site.components.card')
       @endcomponent
     </div>

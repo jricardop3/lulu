029e9a613eecfe45925fca142689fc6ee6d95ebb
Gabriel Martinelli <gabrielmartinellix@outlook.com>
2024-03-11 22:42:52 -0300

    0.91

--- resources/views/site/agende.blade.php
+++ resources/views/site/agende.blade.php
@@ -1,16 +1,16 @@
 @extends('site.layout.layout')
 @section('title', 'Louise Nutrição')
 @section('conteudo')
-
-<div class="row gx-0">
+<div class="container-fluid row">
+<div class="col-md-6 col-sm-12 gx-0">
       @component('site.components.input')
       @endcomponent
     </div>
-    <div class="row gx-0">
+    <div class="col-md-6 col-sm-12  gx-0">
       @component('site.components.login')
       @endcomponent
     </div>
-
+    </div>
 <a href="https://www.instagram.com/louiseduarte.nutri/" style="position:fixed;width:60px;height:60px;bottom:40px;right:40px;
 background-color:#833ab4;
 color:#FFF;background: radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%);border-radius:50px;

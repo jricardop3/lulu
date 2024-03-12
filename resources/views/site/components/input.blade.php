b563d3b0bd853d64d096b493cfbc7a76e509f6e9
Manatocode <gabrielmartinellix@outlook.com>
2024-03-12 16:05:08 -0300

    0.92

--- resources/views/site/components/input.blade.php
+++ resources/views/site/components/input.blade.php
@@ -1,23 +1,20 @@
-<form class="input-group" style="height: 75% ; width: 100% ">
-    <div class="container-fluid row">
-        <div class="col-md-8" >
-            <label for="exampleFormControlInput1" class="form-label" >Nome:</label>
+<form class="input-group d-block px-3" style="width: 100% ;">
+    <div class="row" >
+        <div class="col-md-8">
+            <label for="exampleFormControlInput1" class="form-label">Nome:</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Digite seu nome aqui">
         </div>
-        <div class="col">
+        <div class="col-md-4">
             <label for="exampleFormControlInput2" class="form-label">Telefone:</label>
             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="(99)-9999-9999">
         </div>
     </div>
-    
-    <div class="row" style="width: 98%;">
-    <div class="mb-3" >
-        <label for="exampleFormControlTextarea1" class="form-label">Conte o seu caso:</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        <br>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+    <div class="" >
+        <div class="">
+            <label for="exampleFormControlTextarea1" class="form-label">Conte o seu caso:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+            <br>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </div>
     </div>
-    </div>
-
-
 </form>
